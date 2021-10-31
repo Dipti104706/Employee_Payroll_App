7be1efcb50bb1989d5841fcc5658fff60f771680
Dipti104706 <diptimayeebehura104@gmail.com>
2021-10-31 23:46:46 +0530

    [Dipti] added code to create object on performing save operation

--- js/EmployeeRegister.js
+++ js/EmployeeRegister.js
@@ -30,3 +30,72 @@ window.addEventListener('DOMContentLoaded',(event)=>
     output.textContent = salary.value;
     });
 });
+
+/*Uc3 - create employee payroll object on doing save operation */
+const save=()=>
+{
+    try
+    {
+        let employeePayrollData=createEmployeePayroll();
+    }
+    catch(ex)
+    {
+        return;
+    }
+}
+
+
+const createEmployeePayroll=() =>
+{
+    let employeePayrollData=new EmployeePayrollData();
+    try
+    {
+        employeePayrollData.name=getInputValueById("#name");
+    }
+    catch(e)
+    {
+        setTextValue(".text-error",e);
+        throw e;
+    }
+    employeePayrollData.profilePic=getSelectedValues('[name=profile]').pop(); /*pop() converts array to variable(like profilepic has 4 option , the option which got checked only that is taken by pop()) */
+    employeePayrollData.gender=getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department=getSelectedValues('[name=department]');
+    employeePayrollData.salary=getInputValueById("#salary");
+    employeePayrollData.notes=getInputValueById("#notes");
+    let date=getInputValueById("#day")+ " "+getInputValueById("#month") + " "+getInputValueById("#year");
+    try
+    {
+        employeePayrollData.startDate=new Date(Date.parse(date));
+    }
+    catch(e)
+    {
+        setTextValue(".date-error",e);
+        throw e;  
+    }
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+
+
+const getSelectedValues=(propertyValue)=>
+{
+    let allItems=document.querySelectorAll(propertyValue);
+    let selItems=[];
+    allItems.forEach(item=>
+    {
+        if(item.checked) selItems.push(item.value);
+    });
+    return selItems;
+}
+
+const getInputValueById=(id) =>
+{
+    let value=document.querySelector(id).value;
+    return value;
+}
+
+const setTextValue=(id,value) =>
+{
+    const element=document.querySelector(id);
+    element.textContent=value;
+}
